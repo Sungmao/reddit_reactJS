03a8e741c06a9056538a603743945a7db7e01004
Wu Sung-Mao <Allen@wus-macbook-pro.attlocal.net>
2017-01-15 22:06:23 -0600

    add title component

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
 
 export default class Main extends Component {
+
+	constructor() {
+		super();
+		this.state = {
+			titles: ["The first post", "The 2nd post"],
+			title: ""
+		}
+		this.handleChange = this.handleChange.bind(this);
+    	this.handleAddTitle = this.handleAddTitle.bind(this);
+	}
+
+	handleChange(event) {
+    this.setState({
+      title: event.target.value
+    });
+  }
+
+	handleAddTitle() {
+		const titles = this.state.titles.concat(this.state.title);
+		this.setState({
+			titles: titles,
+			title:""
+		})
+	}
+
 	render() {
 		console.log("main....");
 		return (
@@ -10,6 +35,13 @@ export default class Main extends Component {
 	      </div>
 
 	      <div className="row">
+	      	<ol>
+	      	{this.state.titles}
+	      	</ol>
+	      	<input value={this.state.title} onChange={this.handleChange} type="text" />
+        	<button onClick={this.handleAddTitle}>Add Title</button>
+
+
 	        {/* This code will dump the correct Child Component */}
 	        {this.props.children}
 	      </div>
--- app/components/subreddit/ListItem.js
+++ app/components/subreddit/ListItem.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 
 export default class ListItem extends Component {
+
+	getInitialState() {
+    return {
+      number: 0
+    };
+  }
+
+
 	render() {
 		return (
 			<li>
+				<h1>test child working</h1>
+				<h1>{this.state.number +2}</h1>
 				<h2>{this.props.post.title}</h2>
 				<p>{this.props.post.content}</p>
 			</li>
